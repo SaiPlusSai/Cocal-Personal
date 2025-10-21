b76855cc956d564a7540cc0b3ca64c6d1382a955
Sergio_Arias <148729515+SaiPlusSai@users.noreply.github.com>
2025-10-21 18:25:48 -0400

    base de datos
    base de datos

--- cocal_personal/lib/main.dart
+++ cocal_personal/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'servicios/supabase_service.dart'; // 👈 importa el servicio
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SupabaseService.inicializar(); // 👈 corregido
   runApp(const AplicacionCoCal());
 }
