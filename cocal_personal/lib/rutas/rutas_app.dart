234ed226212fcb5f517454f46ef6d56f2e8d5deb
Sergio_Arias <148729515+SaiPlusSai@users.noreply.github.com>
2025-10-21 20:51:36 -0400

    fin
    fin

--- cocal_personal/lib/rutas/rutas_app.dart
+++ cocal_personal/lib/rutas/rutas_app.dart
@@ -2,11 +2,7 @@ import 'package:flutter/material.dart';
 import '../pantallas/inicio/pantalla_inicio.dart';
 import '../pantallas/autenticacion/pantalla_login.dart';
 
-/// 🔹 Mapa global de rutas de navegación de CoCal
-/// 
-/// Notá que NO se incluye `PantallaPrincipal` aquí,
-/// porque requiere un parámetro (correo) que solo se obtiene
-/// al iniciar sesión correctamente.
+
 Map<String, WidgetBuilder> obtenerRutas() {
   return {
     '/inicio': (context) => const PantallaInicio(),
